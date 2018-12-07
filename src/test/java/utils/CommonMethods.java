c6a2d3ec104ca98425514a5a3c9884a91298d6c8
shokriyan <mohd.shokriyan@gmail.com>
2018-12-07 11:46:51 -0500

    First initial

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class CommonMethods extends BaseClass{
+	
+	public static void sendFieldValue(WebElement element , String value) {
+		element.clear();
+		element.sendKeys(value);
+	}
+	
+	public static void clickOnButtons(WebElement element) {
+		clickableWait(element);
+		element.click();
+	}
+	
+	public static void clickOnlinks (String linkText) {
+		WebElement element = driver.findElement(By.linkText(linkText));
+		clickableWait(element);
+		element.click();
+	}
+
+}
---
+++ src/test/java/utils/ConstanceClass.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class ConstanceClass {
+	
+	
+	public static final String configFilePath = System.getProperty("user.dir")+"/Configuration/credential.properties";
+	
+	
+
+}

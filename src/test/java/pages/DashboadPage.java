0fd7b104ad5a63e61cfc068a15afb615046f193d
shokriyan <mohd.shokriyan@gmail.com>
2018-12-07 14:54:09 -0500

    Dashboard page created

---
+++ src/test/java/pages/DashboadPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.BaseClass;
+
+public class DashboadPage extends BaseClass{
+	
+	@FindBy (how = How.ID, using = "welcome")
+	public WebElement loggeduser; 
+	
+	public DashboadPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String getLoggedUser() {
+		String user = loggeduser.getText();
+		return user.substring(8);
+	}
+
+}

e99f11292216ccdf84a19d307e0aa2433ebad996
shokriyan <mohd.shokriyan@gmail.com>
2018-12-08 14:04:20 -0500

    Credential changes in utility

--- src/test/java/utils/ConstanceClass.java
+++ src/test/java/utils/ConstanceClass.java
@@ -4,7 +4,6 @@ public class ConstanceClass {
 	
 	
 	public static final String configFilePath = System.getProperty("user.dir")+"/Configuration/credential.properties";
-	public static final String excelFilePath = System.getProperty("user.dir")+"testdata/testdata.xlsx";
 	
 	
 
